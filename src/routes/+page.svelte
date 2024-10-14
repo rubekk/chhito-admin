8208e996b35b68a23934cd81e1caa1e78ace2109
Rubek Maharjan <rubekmhzn7@gmail.com>
2024-10-14 14:49:34 +0545

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import AdminProduct from "./../components/AdminProduct.svelte";
+    import AdminOrder from "./../components/AdminOrder.svelte";
+</script>
+
+<AdminProduct />
+<AdminOrder />
